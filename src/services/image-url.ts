51951aa1cb3fa969ddb20cfbf6409e75f09f7f36
Andre Krisna <akrisna@gmail.com>
2024-10-15 02:01:49 -0700

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  let result = url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  console.log(result);
+  return result;
+};
+
+export default getCroppedImageUrl;
